d8bfbb432b1fb7df8a7bee4749330ed5d8a1c872
wel193 <teresalin8405@gmail.com>
2021-12-08 13:26:20 -0800

    rebase master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 import './Vendors/bootstrap.min.css';
 import DetailsScreen from "./Components/DetailsScreen";
 import HomeScreen from "./Components/HomeScreen";
@@ -9,16 +9,16 @@ import RegisterScreen from './Components/RegisterScreen'
 
 function App() {
   return (
-    <div className="container">
+      <div className="container">
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomeScreen/>}></Route>
-                <Route path="/details/:id" element={<DetailsScreen/>}></Route>
-              <Route path="/login" element={<LoginScreen/>}></Route>
-              <Route path="/register" element={<RegisterScreen/>}></Route>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>}></Route>
+            <Route path="/details/:id" element={<DetailsScreen/>}></Route>
+            <Route path="/login" element={<LoginScreen/>}></Route>
+            <Route path="/register" element={<RegisterScreen/>}></Route>
+          </Routes>
         </BrowserRouter>
-    </div>
+      </div>
   );
 }
 
